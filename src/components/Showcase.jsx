3e3ead4e9a5b22b2c23bc2668c9540354a656703
Code bee <codebee345@outlook.com>
2024-04-15 09:09:14 +0100

    modified hero section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,9 +1,9 @@
 const Showcase = (props) => {
 
   return (
-    <div className={`p-4 sm:p-7 odd:bg-[#F3F3F3] even:bg-[#D9E1E7] flex flex-col items-center w-[33%]`}>
-        <p className="text-dark-blue font-bold text-2xl sm:text-4xl font-worksans">{props.value}</p>
-        <p className="text-black text-[0.8rem] text-center mt-2 font-worksans">{props.label}</p>
+    <div className={`p-4 sm:p-7 odd:bg-[#F3F3F3] even:bg-[#D9E1E7] flex flex-col items-center justify-center w-[33%] h-[110px]`}>
+        <p className="text-dark-blue font-bold text-2xl sm:text-4xl font-outfit">{props.value}</p>
+        <p className="text-black text-[0.8rem] text-center mt-2 font-outfit">{props.label}</p>
     </div>
   )
 }
--- src/components/ShowcaseList.jsx
+++ src/components/ShowcaseList.jsx
@@ -2,7 +2,7 @@ import Showcase from "./Showcase"
 
 const ShowcaseList = () => {
   return (
-    <div className="flex flex-row items-center mt-4 w-[90%] max-w-[500px]">
+    <div className="flex flex-row items-center mt-4 w-[95%] max-w-[700px]">
         <Showcase value='200+' label='Properties owned'/>
         <Showcase value='50+' label='Satisfied customers'/>
         <Showcase value='40+' label='Active agents'/>
